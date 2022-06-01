bf17ca6310d614d946235fd7a9c886fe2562e81a
Kyle Henriksen <khenriksenbootcamp@gmail.com>
2022-05-31 20:45:09 -0700

    updated footer

--- src/pages/footer/footer.js
+++ src/pages/footer/footer.js
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import github from '../../images/GitHub-Logo.png'
+import linked from '../../images/linkedin.png'
 function Footer() {
     return (
-        <div>footer</div>
+        <div class="text-danger text-center" >
+
+            <a href="https://www.github.com/starbjornx"><img src={github} style={{ width: "5%", cursor: "pointer" }} alt=""></img></a>
+            <a href="https://www.linkedin.com/in/kyle-henriksen-40ba4b228/"><img src={linked} style={{ width: "3%", cursor: "pointer" }} alt=""></img></a>
+
+        </div >
     )
 }
 
